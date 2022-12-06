7f99e310b48ef1389f5281eb9bc9641d0a07f929
adamantdude <adam.plee23@gmail.com>
2022-12-06 10:43:03 -0600

    database name at the top

--- database.sql
+++ database.sql
@@ -1,8 +1,6 @@
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
+-- DATABASE NAME : womens_health_app
+
 CREATE TABLE "age_range" ( -- FOR QUESTIONS SEARCH AND CARE GUIDELINES
     "id" SERIAL PRIMARY KEY,
     "age" VARCHAR
