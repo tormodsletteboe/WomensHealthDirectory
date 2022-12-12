41e8d9061cdc3caf6566f6753f004a8b7ff3e685
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-12-12 11:46:47 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -1,87 +1,199 @@
+CREATE TABLE "user" (
+	"id" serial NOT NULL,
+	"username" varchar(255) NOT NULL UNIQUE,
+	"password" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL UNIQUE,
+	"age" int NOT NULL,
+	"zip_code" int NOT NULL,
+	"access_level" int NOT NULL,
+	CONSTRAINT "user_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
 
--- DATABASE NAME : womens_health_app
 
-CREATE TABLE "age_range" ( -- FOR QUESTIONS SEARCH AND CARE GUIDELINES
-    "id" SERIAL PRIMARY KEY,
-    "age" VARCHAR
-); -- THIS TABLE IS STATIC. DOES NOT CHANGE PROGRAMICALLY
 
-CREATE TABLE "health_category" ( -- FOR QUESTIONS SEARCH
-    "id" SERIAL PRIMARY KEY,
-    "category" VARCHAR
-); -- THIS TABLE IS STATIC. DOES NOT CHANGE PROGRAMICALLY
+CREATE TABLE "health_category" (
+	"id" serial NOT NULL,
+	"category" varchar(255) NOT NULL,
+	CONSTRAINT "health_category_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
 
-CREATE TABLE "playbook" ( -- QUESTIONS TO ASK THE DOCTOR
-    "id" SERIAL PRIMARY KEY,
-    "question" VARCHAR,
-    "age_range_id" INT REFERENCES "age_range",
-    "category_id" INT REFERENCES "health_category"
-); -- INSERT INTO "playbook" ("question", "age_range_id", "category_id") VALUES ("Q?", ##, ##);
--- ADMIN can add, remove, edit
--- playbook.router.js
 
-CREATE TABLE "care_guideline" ( -- CARE TIMELINE TO FOLLOW
-    "id" SERIAL PRIMARY KEY,
-    "info" VARCHAR,
-    "age_id" INT REFERENCES "age_range"
-); -- INSERT INTO "care_guideline" ("info", "age_id") VALUES ("I?", ##);
--- ADMIN can add, remove, edit
--- guideline.router.js
 
-CREATE TABLE "user" ( -- USER INFORMATION
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR,
-    "password" VARCHAR,
-    "email" VARCHAR,
-    "age_range_id" INT REFERENCES "age_range",
-    "zip_code" INT
-); -- USER is created upon registration. EMAIL, AGE_RANGE_ID, ZIP_CODE should be editable.
--- user.router.js
+CREATE TABLE "medical_links" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"link" varchar(255) NOT NULL,
+	"logo_url" varchar(1024),
+	CONSTRAINT "medical_links_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
 
-CREATE TABLE "document" ( -- DOCUMENTS TO UPLOAD / DOWNLOAD
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR,
-    "file" VARCHAR
-); -- ADMIN can add, remove
--- document.router.js
 
-CREATE TABLE "physician" ( -- PHYSICIANS TO BE DISPLAYED
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR,
-    "email" VARCHAR
-); -- ADMIN can add, remove, edit
--- physician.router.js
+
+CREATE TABLE "virtuahealth" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"info_cost" varchar(1024),
+	"link" varchar(255) NOT NULL,
+	"specialty" varchar(255),
+	"logo_url" varchar(1000),
+	CONSTRAINT "virtuahealth_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
 
 CREATE TABLE "feedback" (
-    "id" SERIAL PRIMARY KEY,
-    "rating" INT,
-    "comment" VARCHAR,
-    "user_id" INT REFERENCES "user"
-); -- USER adds rows when submitting a feedback form. user_id = req.user.id;
--- ADMIN can remove (?)
--- feedback.router.js
+	"id" serial NOT NULL,
+	"rating" int NOT NULL,
+	"comment" varchar(255) NOT NULL,
+	"user_id" int NOT NULL UNIQUE,
+	CONSTRAINT "feedback_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
 
-INSERT INTO "age_range" ("age") VALUES ("21-25"), ("26-30"), ("31-35"), ("36-40"), ("41-45"), ("46-50"), ("51-55"), ("56-60");
-/*
-   AGE    RANGE
-    1     21 - 25
-    2     26 - 30
-    3     31 - 35
-    4     36 - 40
-    5     41 - 45
-    6     46 - 50
-    7     51 - 55
-    8     56 - 60
-*/
 
-INSERT INTO "health_category" ("category") VALUES ("gynecology"), ("cancers"), ("hormone therapy"), ("diseases")
-/*
-    ID      CATEGORY
-    1       gynecology
-    2       cancers
-    3       hormone therapy
-    4       osteoporosis
-    5       heart disease
-    ... and so on
-*/
+
+CREATE TABLE "faq" (
+	"id" serial NOT NULL,
+	"question" varchar(1024) NOT NULL,
+	"answer" varchar(1024) NOT NULL,
+	"health_category_id" int NOT NULL,
+	"age_range_id" int NOT NULL,
+	CONSTRAINT "faq_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "feedback_q" (
+	"id" serial NOT NULL,
+	"question" varchar(255) NOT NULL,
+	"answer" varchar(255) NOT NULL,
+	"feedback_id" int NOT NULL,
+	CONSTRAINT "feedback_q_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "diagnostic_tool" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"info" varchar(1024) NOT NULL,
+	"health_category_id" int NOT NULL,
+	"age_range_id" int NOT NULL,
+	CONSTRAINT "diagnostic_tool_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "newsletter" (
+	"id" serial NOT NULL,
+	"email" varchar(255) NOT NULL UNIQUE,
+	CONSTRAINT "newsletter_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "age_range" (
+	"id" serial NOT NULL,
+	"low" int NOT NULL,
+	"high" int NOT NULL,
+	CONSTRAINT "age_range_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "doctor_questions" (
+	"id" serial NOT NULL,
+	"question" varchar(1024) NOT NULL,
+	"answer" varchar(1024) NOT NULL,
+	"health_category_id" int NOT NULL,
+	"age_range_id" int NOT NULL,
+	CONSTRAINT "doctor_questions_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "resources" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"link" varchar(1024) NOT NULL,
+	"description" varchar(1024) NOT NULL,
+	"health_category_id" int NOT NULL,
+	CONSTRAINT "resources_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "guidelines" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"info" varchar(1024) NOT NULL,
+	"health_category_id" int NOT NULL,
+	"age_range_id" int NOT NULL,
+	"grade" varchar(255),
+	"date" DATE,
+	CONSTRAINT "guidelines_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+
+
+ALTER TABLE "feedback" ADD CONSTRAINT "feedback_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+
+ALTER TABLE "faq" ADD CONSTRAINT "faq_fk0" FOREIGN KEY ("health_category_id") REFERENCES "health_category"("id");
+ALTER TABLE "faq" ADD CONSTRAINT "faq_fk1" FOREIGN KEY ("age_range_id") REFERENCES "age_range"("id");
+
+ALTER TABLE "feedback_q" ADD CONSTRAINT "feedback_q_fk0" FOREIGN KEY ("feedback_id") REFERENCES "feedback"("id");
+
+ALTER TABLE "diagnostic_tool" ADD CONSTRAINT "diagnostic_tool_fk0" FOREIGN KEY ("health_category_id") REFERENCES "health_category"("id");
+ALTER TABLE "diagnostic_tool" ADD CONSTRAINT "diagnostic_tool_fk1" FOREIGN KEY ("age_range_id") REFERENCES "age_range"("id");
+
+
+
+ALTER TABLE "doctor_questions" ADD CONSTRAINT "doctor_questions_fk0" FOREIGN KEY ("health_category_id") REFERENCES "health_category"("id");
+ALTER TABLE "doctor_questions" ADD CONSTRAINT "doctor_questions_fk1" FOREIGN KEY ("age_range_id") REFERENCES "age_range"("id");
+
+ALTER TABLE "resources" ADD CONSTRAINT "resources_fk0" FOREIGN KEY ("health_category_id") REFERENCES "health_category"("id");
+
+ALTER TABLE "guidelines" ADD CONSTRAINT "guidelines_fk0" FOREIGN KEY ("health_category_id") REFERENCES "health_category"("id");
+ALTER TABLE "guidelines" ADD CONSTRAINT "guidelines_fk1" FOREIGN KEY ("age_range_id") REFERENCES "age_range"("id");
+
+
+
+
+
+
+
+
+
+
+
+
 
