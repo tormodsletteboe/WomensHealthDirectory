931459a022a8b62bbe0854a22361d000a4af643b
Tia Theisen <tiatheisen@gmail.com>
2022-12-13 12:41:18 -0600

    update agerange with map

--- src/components/PreventativeCare/PreventativeCare.jsx
+++ src/components/PreventativeCare/PreventativeCare.jsx
@@ -11,6 +11,10 @@ function PreventativeCare() {
     const healthCategories = useSelector((store)=>{
         return store.healthCategories;
     })
+    const ageRanges = useSelector((store)=>{
+        return store.ageRanges;
+    })
+    console.log('age ranges is', ageRanges);
 
     //fetch user's age on page load to autofill form with user's age
     //TODO: update database (add user_id foreign key to healthcategory table), create new saga function and reducer
@@ -19,6 +23,9 @@ function PreventativeCare() {
         dispatch({
             type: 'FETCH_USER_AGE'
         });
+        dispatch({
+            type: 'FETCH_AGE_RANGES'
+        })
     }, []);
 
     const handleAgeChange  = (event) => {
@@ -32,6 +39,7 @@ function PreventativeCare() {
         dispatch({
             type: 'FETCH_HEALTH_CATEGORIES'
         })
+
     }
     
     //When a category is clicked, it will go to a detail view of the id of the button clicked
@@ -45,25 +53,15 @@ function PreventativeCare() {
     <h4>Recommended Screening Guidelines</h4>
 
     <form onSubmit={handleAgeFormSubmit}>
-                <select onChange={handleAgeChange}>
-                    <option defaultValue="0">Select Age</option>
-                    <option value="15-19" >15-19</option>
-                    <option value="20-24" >20-24</option>
-                    <option value="25-29" >25-29</option>
-                    <option value="30-34">30-34</option>
-                    <option value="35-39">35-39</option>
-                    <option value="40-44">40-44</option>
-                    <option value="45-49">45-49</option>
-                    <option value="50-54">50-54</option>
-                    <option value="55-59">55-59</option>
-                    <option value="60-64">60-64</option>
-                    <option value="65-69">65-69</option>
-                    <option value="">42</option>
-                    <option value="43">43</option>
-                    <option value="44">44</option>
-                    <option value="45">45</option>
-                </select>
-                <Button variant="contained" style={{backgroundColor:'#276359'}} type="submit">Submit</Button>
+            <select name="agerange" id="ageRangeSelect">
+                <option defaultValue="Choose Your Age Range">Choose Your Age Range</option>
+            {ageRanges.map(ageRange =>
+                (<option key={ageRange.id} value={`{ageRange.low} - {ageRange.high}`}>
+                    {ageRange.low} - {ageRange.high}
+                </option>
+                 ))}
+            </select>
+            <Button variant="contained" style={{backgroundColor:'#276359'}} type="submit">Submit</Button>
     </form>
     
     {healthCategories.map(category => (
--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -3,7 +3,7 @@ import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import healthCategoriesSaga from './healthCategoriesSaga';
-import ageRangeSaga from './agerange.saga';
+import ageRangeSaga from './ageRange.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
