d3d3b89cf0dd96d0f55273389861e2e4ec225412
AlinaTrukhina <alina.trukhina@gmail.com>
2022-12-13 12:37:40 -0600

    add dropdown

--- src/components/AdminPreventativeCare/AdminPreventativeCare.jsx
+++ src/components/AdminPreventativeCare/AdminPreventativeCare.jsx
@@ -6,27 +6,37 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-
-
 function AdminPreventativeCare() {
 
     const dispatch = useDispatch();
 
+    // stores
     const ageRanges = useSelector(store => store.ageRanges);
+
+    // state vars
+    const [age, setAge] = React.useState('');
 
     // fetch age ranges
     useEffect(() => {
         dispatch({ type: 'FETCH_AGE_RANGES' });
     }, []);
 
-
     // fetch categories
 
 
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
     return (
         <>
-            <p>age ranges</p>
-
+            <label for="ageRange"> Choose Your Age Range </label>
+            <select name="ageRange" id="ageRangeSelect">
+                {ageRanges.map(ageRange => 
+                (<option key={ageRange.id} value={`{ageRange.low} - {ageRange.high}`}>
+                    {ageRange.low} - {ageRange.high}
+                </option>))}
+            </select>
         </>
     )
 }
