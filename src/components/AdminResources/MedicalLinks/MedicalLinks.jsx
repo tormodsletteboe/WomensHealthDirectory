37506a4d6699c57fd7698e9a45aa7d9c12b874b2
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-12-14 10:36:03 -0600

    working on redux state for edit medical link

--- src/components/AdminResources/MedicalLinks/MedicalLinks.jsx
+++ src/components/AdminResources/MedicalLinks/MedicalLinks.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -11,22 +11,51 @@ function MedicalLinks() {
   // a default value of 'Functional Component'
   //const store = useSelector((store) => store);
   const [heading, setHeading] = useState("Medical Links");
-  const [title,setTitle]=useState('');
-  const [url,setUrl]=useState('');
-  const [urlLogo,setUrlLogo]=useState('');
-  const handleClick = ()=>{
-    console.log('title',title);
-    console.log('url',url);
-    console.log('urllogo',urlLogo);
-  }
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [urlLogo, setUrlLogo] = useState("");
+
+  const dispatch = useDispatch();
+  const editMedLinks = useSelector(store=>store.editMedicalLinks);
+  const handleClick = () => {
+    console.log("title", title);
+    console.log("url", url);
+    console.log("urllogo", urlLogo);
+  };
   return (
     <Box>
-      <form>
-        <TextField label="Title" variant="outlined" multiline maxRows={4} value={title} onChange={()=>setTitle(event.target.value)} />
-        <TextField label="Url" variant="outlined" multiline maxRows={4} value={url} onChange={()=>setUrl(event.target.value)} />
-        <TextField label="Logo Url" variant="outlined" multiline maxRows={4} value={urlLogo} onChange={()=>setUrlLogo(event.target.value)} />
+      
+        <TextField
+          label="Title"
+          variant="outlined"
+          multiline
+          maxRows={4}
+          value={editMedLinks.title}
+          onChange={(event) =>
+            dispatch({
+              type: "SET_MEDICAL_TITLE",
+              payload: event.target.value
+            })
+          }
+        />
+        <TextField
+          label="Url"
+          variant="outlined"
+          multiline
+          maxRows={4}
+          value={url}
+          onChange={(event) => setUrl(event.target.value)}
+        />
+        <TextField
+          label="Logo Url"
+          variant="outlined"
+          multiline
+          maxRows={4}
+          value={urlLogo}
+          onChange={(event) => setUrlLogo(event.target.value)}
+        />
         <Button onClick={handleClick}>Click me</Button>
-      </form>
+      
       <div></div>
     </Box>
   );
