a51c66ce60fbc112bc4eae7967641f81872f0040
AlinaTrukhina <alina.trukhina@gmail.com>
2022-12-15 13:24:53 -0600

    add selected age range reducer

---
+++ src/redux/reducers/selectedAgeRange.reducer.js
@@ -0,0 +1,13 @@
+const selectedAgeRange = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_SELECTED_AGE_RANGE':
+      return action.payload;
+    case 'RESET_SELECTED_AGE_RANGE':
+      return '';
+    default:
+      return state;
+  }
+};
+  
+
+export default resourceToEdit;
