a123a2dabf0256e70f9d462ac86e50e06e09c8bd
Tia Theisen <tiatheisen@gmail.com>
2023-01-02 11:01:16 -0600

    add styling to resources

--- src/components/Resources/Resources.jsx
+++ src/components/Resources/Resources.jsx
@@ -1,7 +1,23 @@
+import {useHistory} from 'react-router-dom';
+import Button from '@mui/material/Button';
+import './Resources.css';
 
 function Resources(){
+
+    const virtualHealth = () => {
+        history.pushState('/virtualhealth');
+    }
+
+    const medicalLinks = () => {
+        history.pushState('/medicallinks');
+    }
+
     return(
     <>
+    <div className = "resources">
+    <Button className="virtualHealth" onClick = {virtualHealth} variant="contained" style={{backgroundColor:'#276359'}}>Virtual Health</Button>
+    <Button className = "medicalLinks" onClick = {medicalLinks} variant="contained" style={{backgroundColor:'#276359'}}>Medical Links</Button>
+    </div>
     </>
     );
 }
