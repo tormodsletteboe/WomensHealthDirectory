6c09927f090004ed0817eac33e41c5e262a3c998
Tormod Sletteboe <tormod.slettebo@gmail.com>
2023-01-03 09:54:17 -0600

    trying to get cards to display

--- src/components/UserVirtualHealth/UserVirtualHealth.jsx
+++ src/components/UserVirtualHealth/UserVirtualHealth.jsx
@@ -8,35 +8,70 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-
+import Container from "@mui/material/Container";
 import "./UserVirtualHealth.css";
-
-
 
 //test
 function UserVirtualHealth() {
   const virtualhealthlinks = useSelector((store) => store.virtualhealth_links);
   const dispatch = useDispatch();
 
-useEffect(()=>{
-    dispatch({type: "FETCH_VIRTUALHEALTH_LINKS"});
-},[]);
+  useEffect(() => {
+    dispatch({ type: "FETCH_VIRTUALHEALTH_LINKS" });
+  }, []);
 
-
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+  
 
   return (
-    <>
+  
+    <Container maxWidth="sm">
+      <Box sx={{ bgcolor: '#cfe8fc' }}>
       <div className="virtualHealthTitle">
         <h3>Virtual Health</h3>
       </div>
       <ul>
         {virtualhealthlinks.map((virthealthlink) => (
-          <li>
-           <h2>hello</h2>
+          <li key={virthealthlink.id}>
+            <Card sx={{ minWidth: 275, marginBottom: 2 }} >
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">
+                  be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  adjective
+                </Typography>
+                <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
           </li>
         ))}
       </ul>
-    </>
+      </Box>
+    </Container>
+      
+  
   );
 }
 
