f7fefc21e3eab5d2452bd551ca64b3499d3390e3
Tia Theisen <tiatheisen@gmail.com>
2023-01-05 15:22:33 -0600

    debug feedback saga error

--- src/components/AdminFeedbackView/AdminFeedbackView.jsx
+++ src/components/AdminFeedbackView/AdminFeedbackView.jsx
@@ -1,7 +1,16 @@
 import {useDispatch, useSelector} from 'react-redux';
 import {useEffect} from 'react';
-
-
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 function AdminFeedbackView() {
     const dispatch = useDispatch();
@@ -13,14 +22,45 @@ function AdminFeedbackView() {
     }, []);
 
     const feedback = useSelector((store)=> {
-        
+        //render feedback here
     })
 
 
 
     return(
-        <>
-        <h1>Feedback goes here</h1>
+      <>
+      <div>
+      <Accordion style= {{width: '80%'}}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
 
         </>
     );
--- src/redux/sagas/feedback.saga.js
+++ src/redux/sagas/feedback.saga.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, put } from "redux-saga/effects";
 
 function* fetchUserFeedback() {
 try {
