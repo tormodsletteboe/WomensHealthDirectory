826c2b7ba53b389efee7e482911a0c705ce76f48
Tia Theisen <tiatheisen@gmail.com>
2023-01-05 08:22:23 -0600

    add history.push to feedback button

--- src/components/AdminLandingPage/AdminLandingPage.jsx
+++ src/components/AdminLandingPage/AdminLandingPage.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import './AdminLandingPage.css';
-
 import {useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -13,13 +13,23 @@ function AdminLandingPage() {
   
   //const store = useSelector((store) => store);
   const [heading, setHeading] = useState('Welcome, Admin!');
+  const history = useHistory();
+
+  const handleViewFeedback = () => {
+    history.push('/feedbackview');
+  }
 
   return (
     <div className = "adminLandingPage">
       <h2>{heading}</h2>
-      <Button variant="contained" style = {{backgroundColor: '#8EBBA7', color: '#FFFFFF', fontSize: '18px'}}>View User Feedback</Button>
-
-
+      <Button variant="contained" 
+              style = 
+                {{backgroundColor: '#8EBBA7', 
+                color: '#FFFFFF', 
+                fontSize: '18px'}}
+                onClick = {handleViewFeedback}
+              >View User Feedback
+      </Button>
     </div>
   );
 }
