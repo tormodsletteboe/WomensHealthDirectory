13507e64c6365b7d586457001f1a5f4c30e4ec56
AlinaTrukhina <alina.trukhina@gmail.com>
2023-01-04 20:28:50 -0600

    render list of emails

--- src/components/AdminLandingPage/AdminLandingPage.jsx
+++ src/components/AdminLandingPage/AdminLandingPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { CSVLink, CSVDownload } from "react-csv";
+import { CSVLink } from "react-csv";
 
-import { Button, Stack } from '@mui/material';
+import { Button, Stack, Typography, List, ListItem, Divider } from '@mui/material';
 
 function AdminLandingPage() {
     // hooks
@@ -14,20 +14,44 @@ function AdminLandingPage() {
 	
 	const headers = [{ label: "Email Address", key: "email" }]
 	const [heading, setHeading] = useState('Admin Landing Page');
+	const [isClicked, setClicked] = useState(false);;
 
 	useEffect(() => {
 		dispatch({type: 'FETCH_NEWSLETTER_EMAILS'})
 	}, []);
 
+	function handleEmailClick() {
+		setClicked(!isClicked);
+		console.log('clicked?', isClicked)
+	}
 
 	return (
 		<>
-			<h2>{heading}</h2>
+
+			<Typography component="h1" variant="h3">Welcome, Laina!</Typography>
 			<Stack spacing={2}>
-				<Button variant="contained">
-					View Newsletter Sign-up Emails
-				</Button>
-				<Button variant="contained">
+				{ isClicked === true ?
+				<List>
+					<Button variant="contained" color="primary"
+					onClick={handleEmailClick}
+					> 
+						Close Newsletter Sign-up Emails
+					</Button>
+				{ newsletterEmails.map(x => (
+					<>
+					<ListItem key={x.id}>{x.email}</ListItem>
+					<Divider variant="inset" component="li" />
+					</>
+				))}
+				</List>
+				:
+					<Button variant="contained" color="primary"
+					onClick={handleEmailClick}
+					> 
+						View Newsletter Sign-up Emails
+					</Button> 
+				}
+				<Button variant="contained" color="primary">
 					<CSVLink 
 						data={newsletterEmails} 
 						headers={headers} 
@@ -36,8 +60,6 @@ function AdminLandingPage() {
 					Download Newsletter Emails Addresses
 					</CSVLink>
 				</Button>
-
-				
 			</Stack>
 		</>
 	);
