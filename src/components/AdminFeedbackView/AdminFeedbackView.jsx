9fb284faa8b134ec91caba28bb6ded2e9b848c0c
Tia Theisen <tiatheisen@gmail.com>
2023-01-05 22:38:09 -0600

    render question and answers in accordion in admin feedback view

--- src/components/AdminFeedbackView/AdminFeedbackView.jsx
+++ src/components/AdminFeedbackView/AdminFeedbackView.jsx
@@ -1,5 +1,5 @@
-import {useDispatch, useSelector} from 'react-redux';
-import {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -21,79 +21,102 @@ function AdminFeedbackView() {
         });
     }, []);
 
-    const feedback = useSelector((store)=> {
+    const feedback = useSelector((store) => {
         return store.feedback;
     })
     console.log('feedback is', feedback);
 
-    return(
-      <>
-      <h1>User Feedback</h1>
-      <div>
-      <Accordion style= {{width: '80%'}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-        <Typography>Comments and Overall Rating</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <TableContainer height= '90%'>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Comments</TableCell>
-                        <TableCell>Overall Rating</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                {feedback.commentsAndRatings && feedback.commentsAndRatings.map(feedbackItem =>
-                    <TableRow>
-                        <TableCell>
-                            {feedbackItem.comment}
-                        </TableCell>
-                        <TableCell>
-                            {feedbackItem.rating}
-                        </TableCell>
-                    </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-          </TableContainer>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion style= {{width: '80%'}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Questions</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-    
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+    return (
+        <>
+            <h1>User Feedback</h1>
+            <div>
+                <Accordion style={{ width: '80%' }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography>Comments and Overall Rating</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <TableContainer height='90%'>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Comments</TableCell>
+                                        <TableCell>Overall Rating</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {feedback.commentsAndRatings && feedback.commentsAndRatings.map(feedbackItem =>
+                                        <TableRow key={feedbackItem.id}>
+                                            <TableCell>
+                                                {feedbackItem.comment}
+                                            </TableCell>
+                                            <TableCell>
+                                                {feedbackItem.rating}
+                                            </TableCell>
+                                        </TableRow>
+                                    )}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </AccordionDetails>
+                </Accordion>
 
-      <Accordion style= {{width: '80%'}}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Most Common Ratings</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </div>
+                <Accordion style={{ width: '80%' }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel2a-content"
+                        id="panel2a-header"
+                    >
+                        <Typography>Most Common Ratings</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+                <div>
+                    {feedback.questionsAndAnswers && feedback.questionsAndAnswers.map(questionAndAnswerItem =>
+                        // {feedback.questionsAndAnswers.json_agg && feedback.questionsAndAnswers.json_agg.map(answerItem =>
+                        <Accordion style={{ width: '80%' }}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2a-content"
+                                id="panel2a-header"
+                            >
+                                <Typography key={questionAndAnswerItem.id}>
+                                    {questionAndAnswerItem.question}
+                                </Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <TableContainer height='90%'>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Answers</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {questionAndAnswerItem.json_agg.map(answer =>
+                                                <TableRow>
+                                                    <TableCell>
+                                                        {answer}
+                                                    </TableCell>
+                                                </TableRow>
+                                            )}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </AccordionDetails>
+                        </Accordion>
+                    )}
+                </div>
+            </div>
+
 
         </>
     );
