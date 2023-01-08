d21cc84a7ba71015bc32654e9dfc7c893d4d32b8
Tormod Sletteboe <tormod.slettebo@gmail.com>
2023-01-08 16:06:01 -0600

    google analytics Link

--- src/components/AdminLandingPage/AdminLandingPage.jsx
+++ src/components/AdminLandingPage/AdminLandingPage.jsx
@@ -31,6 +31,7 @@ import {
 } from "suneditor/src/plugins";
 import Swal from "sweetalert2";
 import { Grid } from "@mui/material";
+import Link from "@mui/material/Link";
 
 function AdminLandingPage() {
   //add code comment
@@ -73,33 +74,33 @@ function AdminLandingPage() {
   // 		console.log(targetImgElement, index, state, imageInfo, remainingFilesCount)
   // }
 
-	const handleViewFeedback = () => {
-		history.push('/adminfeedbackview');
-	}
+  const handleViewFeedback = () => {
+    history.push("/adminfeedbackview");
+  };
 
-	function handleEditorSave() {
-		console.log("in save button, content html:", editorState);
-		let emailList = [];
-		for (let email of newsletterEmails) {
-		  emailList.push(email.email);
-		}
-		console.log("email list is:", emailList);
-		dispatch({
-		  type: "SEND_NEWSLETTER",
-		  payload: { htmlToSend: editorState, emailList: emailList },
-		});
-		Swal.fire({
-		  icon: "success",
-		  title: "Emails have been sent!",
-		  showConfirmButton: false,
-		  timer: 1500,
-		});
-		handleEditorClick();
-	  }
+  function handleEditorSave() {
+    console.log("in save button, content html:", editorState);
+    let emailList = [];
+    for (let email of newsletterEmails) {
+      emailList.push(email.email);
+    }
+    console.log("email list is:", emailList);
+    dispatch({
+      type: "SEND_NEWSLETTER",
+      payload: { htmlToSend: editorState, emailList: emailList },
+    });
+    Swal.fire({
+      icon: "success",
+      title: "Emails have been sent!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    handleEditorClick();
+  }
 
-	function handleNewsletterDemoClick() {
-		console.log('clicked');
-		SunEditor.defaultValue = `<h1>Welcome to the ViFi Newsletter!</h1>
+  function handleNewsletterDemoClick() {
+    console.log("clicked");
+    SunEditor.defaultValue = `<h1>Welcome to the ViFi Newsletter!</h1>
 
 		<p>Thank you for signing up! Now you will find all the latest women&apos;s health news in your inbox!</p>
 		
@@ -141,110 +142,148 @@ function AdminLandingPage() {
 		</p>
 		</div>
 		
-		<footer align="center">Copyright 2023 ViFi</footer>`
-	}
+		<footer align="center">Copyright 2023 ViFi</footer>`;
+  }
 
   return (
-    <Container >
+    <Container>
       <Grid
         container
         direction="column"
         justifyContent="center"
         alignItems="center"
-		columnSpacing={2}
+        columnSpacing={2}
       >
         <Grid item>
           <Typography component="h1" variant="h3" align="center" m="20px">
             Welcome, Admin!
           </Typography>
         </Grid>
-		<Grid item mb={2} >
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleViewFeedback}
-        >
-          View User Feedback
-        </Button>
-		</Grid>
         <Grid item mb={2}>
-          <Button variant="contained" color="primary" onClick={handleEmailClick}>
-            {isEmailListClicked === true
-              ? "Close Newsletter Sign-up Emails"
-              : "View Newsletter Sign-up Emails"} <div className=""></div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleViewFeedback}
+          >
+            View User Feedback
           </Button>
         </Grid>
-        {isEmailListClicked === true ? (
-			<Grid item mb={2}>
-          <List>
-            {newsletterEmails.map((x) => (
-              <>
-                <ListItem key={x.id}>{x.email}</ListItem>
-                <Divider  component="li" />
-              </>
-            ))}
-          </List>
-		  </Grid>
-        ) : null}
 		<Grid item mb={2}>
-        <Button variant="contained" color="primary">
-          <CSVLink
-            data={newsletterEmails}
-            headers={headers}
-            filename={"newsletter-emails.csv"}
+          <Link
+            mr={1}
+            ml={0}
+            pl={1}
+            variant="button"
+            target="_blank"
+            underline="none"
+            href={
+              "https://analytics.google.com/analytics/web/?utm_source=demoaccount&utm_medium=demoaccount&utm_campaign=demoaccount#/report/visitors-flow/a54516992w87479473p92320289/"
+            }
           >
-            Download Newsletter Emails Addresses
-          </CSVLink>
-        </Button>
-		</Grid>
+            {"Google Analytics"}
+          </Link>
+        </Grid>
+        <Grid item mb={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleEmailClick}
+          >
+            {isEmailListClicked === true
+              ? "Close Newsletter Sign-up Emails"
+              : "View Newsletter Sign-up Emails"}{" "}
+            <div className=""></div>
+          </Button>
+        </Grid>
+        
+        {isEmailListClicked === true ? (
+          <Grid item mb={2}>
+            <List>
+              {newsletterEmails.map((x) => (
+                <>
+                  <ListItem key={x.id}>{x.email}</ListItem>
+                  <Divider component="li" />
+                </>
+              ))}
+            </List>
+          </Grid>
+        ) : null}
+        <Grid item mb={2}>
+          <Button variant="contained" color="primary">
+            <CSVLink
+              data={newsletterEmails}
+              headers={headers}
+              filename={"newsletter-emails.csv"}
+            >
+              Download Newsletter Emails Addresses
+            </CSVLink>
+          </Button>
+        </Grid>
 
         {isEditorClicked === false ? (
           <Grid item mb={2}>
-		  <Button variant="contained" onClick={handleEditorClick}>
-            Write Newsletter
-          </Button>
-		  </Grid>
+            <Button variant="contained" onClick={handleEditorClick}>
+              Write Newsletter
+            </Button>
+          </Grid>
         ) : null}
 
-		{ isEditorClicked === true ?
-			<Box margin="20px 0px" display='flex' 
-			alignItems="center" justifyContent="center"
-			flexDirection="column">
-				<SunEditor name='newsletter-editor' 
-					defaultValue=""
-					placeholder="Newsletter editor - add your text here..."
-					width='80%' height='400px'
-					autoFocus={true}
-					onChange={handleChange}
-					// onImageUpload={handleImageUpload}
-					setOptions={{ buttonList: buttonList.formatting, 
-					plugins: [
-						font,
-						fontColor,
-    					fontSize,
-						textStyle,
-						link,
-						image,
-						imageGallery
-    				], buttonList: [
-						['undo', 'redo', 'removeFormat'],
-						['font', 'fontSize', 'fontColor'],
-						['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
-						['align', 'outdent', 'indent'],
-        				['list', 'table', 'link', 'image'],
-						['codeView'],
-						['preview', 'print']
-					]
-				}}
-				/>
-			<Stack margin={2} direction="row" spacing={12}>
-				<Button variant='outlined'
-				onClick={handleEditorClick}>Cancel</Button>
-				<Button variant='contained'
-				onClick={handleEditorSave}>Send to email list</Button>
-			</Stack>
-			</Box>
-         : null }
+        {isEditorClicked === true ? (
+          <Box
+            margin="20px 0px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <SunEditor
+              name="newsletter-editor"
+              defaultValue=""
+              placeholder="Newsletter editor - add your text here..."
+              width="80%"
+              height="400px"
+              autoFocus={true}
+              onChange={handleChange}
+              // onImageUpload={handleImageUpload}
+              setOptions={{
+                buttonList: buttonList.formatting,
+                plugins: [
+                  font,
+                  fontColor,
+                  fontSize,
+                  textStyle,
+                  link,
+                  image,
+                  imageGallery,
+                ],
+                buttonList: [
+                  ["undo", "redo", "removeFormat"],
+                  ["font", "fontSize", "fontColor"],
+                  [
+                    "bold",
+                    "underline",
+                    "italic",
+                    "strike",
+                    "subscript",
+                    "superscript",
+                  ],
+                  ["align", "outdent", "indent"],
+                  ["list", "table", "link", "image"],
+                  ["codeView"],
+                  ["preview", "print"],
+                ],
+              }}
+            />
+            <Stack margin={2} direction="row" spacing={12}>
+              <Button variant="outlined" onClick={handleEditorClick}>
+                Cancel
+              </Button>
+              <Button variant="contained" onClick={handleEditorSave}>
+                Send to email list
+              </Button>
+            </Stack>
+          </Box>
+        ) : null}
       </Grid>
     </Container>
   );
