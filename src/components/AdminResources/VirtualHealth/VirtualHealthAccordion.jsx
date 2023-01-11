013c0e768e470fecdef01f2ad58b204295e09f6f
Tormod Sletteboe <tormod.slettebo@gmail.com>
2023-01-11 17:40:37 -0600

    code comments virtual health

--- src/components/AdminResources/VirtualHealth/VirtualHealthAccordion.jsx
+++ src/components/AdminResources/VirtualHealth/VirtualHealthAccordion.jsx
@@ -15,25 +15,34 @@ import Tooltip from "@mui/material/Tooltip";
 import Avatar from "@mui/material/Avatar";
 import PreviewVirtualHealthCard from "./PreviewVirtualHealthCard";
 
+// VirtualHealthAccordion component
 function VirtualHealthAccordion({ virtualhealthlink }) {
+  //state for the preview toggle button
   const [checked, setChecked] = React.useState(false);
+
+  //ref for the preview card
   const containerRef = React.useRef(null);
+
+  //function to toggle the preview card
   const handleChange = () => {
     setChecked((prev) => !prev);
   };
 
-  //   const store = useSelector((store) => store);
   const dispatch = useDispatch();
   return (
     <>
       <Accordion sx={{ my: 2 }}>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Grid container>
+            {/* icon */}
             <Grid item xs={2}>
-            <Avatar alt="" src={virtualhealthlink.logo_url} sx={{ bgcolor: "white" }} />
-              {/* <img src={virtualhealthlink.logo_url} /> */}
+              <Avatar
+                alt=""
+                src={virtualhealthlink.logo_url}
+                sx={{ bgcolor: "white" }}
+              />
             </Grid>
-
+            {/* title */}
             <Grid item xs={2} my={1} textAlign={"start"}>
               <Typography
                 textAlign={"start"}
@@ -42,11 +51,13 @@ function VirtualHealthAccordion({ virtualhealthlink }) {
                 {virtualhealthlink.name}
               </Typography>
             </Grid>
+            {/* specialty */}
             <Grid item xs={2} my={1} textAlign={"start"}>
               <Typography textAlign={"start"} sx={{ color: "text.secondary" }}>
                 {virtualhealthlink.specialty}
               </Typography>
             </Grid>
+            {/* url */}
             <Grid item xs={6} my={1} textAlign={"start"}>
               <Typography textAlign={"start"} sx={{ color: "text.secondary" }}>
                 {virtualhealthlink.link}
@@ -56,40 +67,52 @@ function VirtualHealthAccordion({ virtualhealthlink }) {
         </AccordionSummary>
         <AccordionDetails>
           <Grid container>
+            {/* cost/coverage info */}
             <Grid item xs={4} my={1} textAlign={"start"}>
-              {virtualhealthlink.info_cost && <Grid container direction={"column"}>
-                <Typography mb={1} textAlign="start">
-                  {
-                    <span style={{ fontWeight: "bold" }}>
-                      {"COST/COVERAGE:"}
-                    </span>
-                  }
-                </Typography>
-                <Typography textAlign="start">
-                  {virtualhealthlink.info_cost}
-                </Typography>
-              </Grid>}
+              {virtualhealthlink.info_cost && (
+                <Grid container direction={"column"}>
+                  <Typography mb={1} textAlign="start">
+                    {
+                      <span style={{ fontWeight: "bold" }}>
+                        {"COST/COVERAGE:"}
+                      </span>
+                    }
+                  </Typography>
+                  <Typography textAlign="start">
+                    {virtualhealthlink.info_cost}
+                  </Typography>
+                </Grid>
+              )}
             </Grid>
+            {/* description */}
             <Grid item xs={6} my={1} textAlign={"start"} ml={2}>
-              {virtualhealthlink.description && <Grid container direction={"column"}>
-                <Typography mb={1} textAlign="start">
-                  {<span style={{ fontWeight: "bold" }}>{"DESCRIPTION:"}</span>}
-                </Typography>
-                <Typography
-                  textAlign={"start"}
-                  sx={{ width: "50%", flexShrink: 0 }}
-                >
-                  {virtualhealthlink.description}
-                </Typography>
-              </Grid>}
+              {virtualhealthlink.description && (
+                <Grid container direction={"column"}>
+                  <Typography mb={1} textAlign="start">
+                    {
+                      <span style={{ fontWeight: "bold" }}>
+                        {"DESCRIPTION:"}
+                      </span>
+                    }
+                  </Typography>
+                  <Typography
+                    textAlign={"start"}
+                    sx={{ width: "50%", flexShrink: 0 }}
+                  >
+                    {virtualhealthlink.description}
+                  </Typography>
+                </Grid>
+              )}
             </Grid>
           </Grid>
+          {/* preview toggle button */}
           <Grid item textAlign={"end"}>
             <ToggleButton onClick={handleChange} selected={checked}>
               <Tooltip title={checked ? "Close" : "Preview"}>
                 <PhoneAndroidIcon />
               </Tooltip>
             </ToggleButton>
+            {/* edit button */}
             <Button
               onClick={() =>
                 dispatch({
@@ -100,6 +123,7 @@ function VirtualHealthAccordion({ virtualhealthlink }) {
             >
               Edit
             </Button>
+            {/* delete button */}
             <Button
               onClick={() => {
                 Swal.fire({
@@ -127,6 +151,7 @@ function VirtualHealthAccordion({ virtualhealthlink }) {
           </Grid>
         </AccordionDetails>
       </Accordion>
+      {/* preview card */}
       <Grid item mr={10} sx={{ display: "flex", justifyContent: "end" }}>
         {checked && (
           <Slide direction="up" in={checked} container={containerRef.current}>
