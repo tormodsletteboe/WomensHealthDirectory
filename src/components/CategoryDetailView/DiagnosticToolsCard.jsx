ade067b211703530cf93b38e802a8cb4ae8104d0
Tia Theisen <tiatheisen@gmail.com>
2023-01-11 14:47:22 -0600

    format diagnostic tools card and doctorQuestions card

--- src/components/CategoryDetailView/DiagnosticToolsCard.jsx
+++ src/components/CategoryDetailView/DiagnosticToolsCard.jsx
@@ -2,19 +2,19 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-function DiagnosticToolsCard ({diagTool}) {
+function DiagnosticToolsCard({ diagTool }) {
     return (
         <>
-        <Card sx={{ minWidth: 275, marginBottom: 2}}>
-            <CardContent>
-                <Typography sx={{textAlign:'left'}}>
-                    <b>{diagTool.name}</b>
-                </Typography>
-                <Typography sx={{textAlign:'left'}}>
-                    {diagTool.info}
-                </Typography>
-            </CardContent>
-        </Card>
+            <Card sx={{ minWidth: 275, marginBottom: 2 }}>
+                <CardContent>
+                    <Typography sx={{ textAlign: 'left' }}>
+                        <b>{diagTool.name}</b>
+                    </Typography>
+                    <Typography sx={{ textAlign: 'left' }}>
+                        {diagTool.info}
+                    </Typography>
+                </CardContent>
+            </Card>
         </>
     );
 }
--- src/components/CategoryDetailView/DoctorQuestionsCard.jsx
+++ src/components/CategoryDetailView/DoctorQuestionsCard.jsx
@@ -3,18 +3,18 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-function DoctorQuestionsCard ({question}) {
+function DoctorQuestionsCard({ question }) {
     console.log('question is', question);
     return (
         <>
-        <Card sx={{ minWidth: 275, marginBottom: 0}}>
-            <CardContent>
-                <Typography sx={{textAlign:'left'}}><b>{question.question_category}</b></Typography>
-                <Typography sx={{textAlign:'left'}}>
-                    {question.question}
-                </Typography>
-            </CardContent>
-        </Card>
+            <Card sx={{ minWidth: 275, marginBottom: 0 }}>
+                <CardContent>
+                    <Typography sx={{ textAlign: 'left' }}><b>{question.question_category}</b></Typography>
+                    <Typography sx={{ textAlign: 'left' }}>
+                        {question.question}
+                    </Typography>
+                </CardContent>
+            </Card>
         </>
     );
 }
