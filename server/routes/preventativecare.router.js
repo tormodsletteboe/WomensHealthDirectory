7afb7d10620fce5f12f6faaa208e9c104641649d
Tia Theisen <tiatheisen@gmail.com>
2023-01-11 14:34:26 -0600

    add comments to preventative care get router and format code

--- server/routes/preventativecare.router.js
+++ server/routes/preventativecare.router.js
@@ -5,12 +5,12 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 //GET router to fetch health categories database
 router.get('/', rejectUnauthenticated, function (req, res) {
-    console.log('in /preventativecare GET router');
+  console.log('in /preventativecare GET router');
 
-    let sqlText = `SELECT * FROM "health_category";`;
-   
-    pool.query(sqlText)
-   
+  let sqlText = `SELECT * FROM "health_category";`;
+
+  pool.query(sqlText)
+
     .then(dbRes => {
       res.send(dbRes.rows);
     })
@@ -20,26 +20,25 @@ router.get('/', rejectUnauthenticated, function (req, res) {
     })
 });
 
+//GET router to fetch screening information based on category id and age range id
 router.get('/:catId/ages/:ageId', rejectUnauthenticated, async (req, res) => {
-    console.log('in preventativecare id router');
+  console.log('in preventativecare id router');
 
-    let faqSQLText = `
+  let faqSQLText = `
     SELECT ("question"), ("answer"), ("id") FROM "faq"
     WHERE "health_category_id" = $1 AND "age_range_id"=$2;`;
 
-    let diagSQLText = `SELECT ("name"), ("info"), ("id") FROM "diagnostic_tool"
+  let diagSQLText = `SELECT ("name"), ("info"), ("id") FROM "diagnostic_tool"
     WHERE "health_category_id" = $1 AND "age_range_id"=$2;`;
 
-    // let guidelinesSQLText = `SELECT ("name"), ("info"), ("grade"), tochar(("date"),"YYYY-MM-DD"), ("id") FROM "guidelines"
-    // WHERE "health_category_id" = $1 AND "age_range_id"=$2;`;
-    let guidelinesSQLText = `SELECT name, info, grade, to_char(date,'MM/DD/YYYY') as date, id FROM guidelines WHERE health_category_id = $1 AND age_range_id=$2;`;
+  let guidelinesSQLText = `SELECT name, info, grade, to_char(date,'MM/DD/YYYY') as date, id FROM guidelines WHERE health_category_id = $1 AND age_range_id=$2;`;
 
 
-    let drQuestionsSQLText = `SELECT ("question_category"), ("question"), ("id") FROM "doctor_questions"
+  let drQuestionsSQLText = `SELECT ("question_category"), ("question"), ("id") FROM "doctor_questions"
     WHERE "health_category_id" = $1 AND "age_range_id"=$2;`;
 
-    // Get category details
-    try{
+  // Get category details
+  try {
 
     //Get FAQs  
     let faqRes = await pool.query(faqSQLText, [req.params.catId, req.params.ageId]);
@@ -55,17 +54,17 @@ router.get('/:catId/ages/:ageId', rejectUnauthenticated, async (req, res) => {
     let drQuestionsRes = await pool.query(drQuestionsSQLText, [req.params.catId, req.params.ageId]);
 
     let apiRes = {
-        faqs: faqRes.rows,
-        diagTools: diagRes.rows,
-        guidelines: guidelinesRes.rows,
-        drQuestions: drQuestionsRes.rows,
+      faqs: faqRes.rows,
+      diagTools: diagRes.rows,
+      guidelines: guidelinesRes.rows,
+      drQuestions: drQuestionsRes.rows,
     }
     res.send(apiRes);
 
-    }catch (err) {
-        console.log('Error with fetching category details', err);
-        res.sendStatus(500);
-    }
+  } catch (err) {
+    console.log('Error with fetching category details', err);
+    res.sendStatus(500);
+  }
 })
 
 router.get('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, async (req, res) => {
@@ -76,8 +75,8 @@ router.get('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, async (req
   let sqlParams = [req.params.catId, req.params.ageId];
   let sqlText = '';
 
-  switch(sectionName) {
-    case 'Guidelines': 
+  switch (sectionName) {
+    case 'Guidelines':
       sqlText = `SELECT "id", "name" AS "field01",
         "info" AS "field02", 
         "grade" AS "field03", to_char(date, 'YYYY-MM-DD') AS "field04" 
@@ -111,13 +110,13 @@ router.get('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, async (req
   // Get category details
   try {
 
-  let dbRes = await pool.query(sqlText, sqlParams);
+    let dbRes = await pool.query(sqlText, sqlParams);
 
-  res.send(dbRes.rows);
+    res.send(dbRes.rows);
 
   } catch (err) {
-      console.log('Error with fetching category details', err);
-      res.sendStatus(500);
+    console.log('Error with fetching category details', err);
+    res.sendStatus(500);
   }
 })
 
@@ -129,41 +128,41 @@ router.put('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, (req, res)
 
   let sqlParams = [req.body.id, req.params.catId, req.params.ageId];
   let sqlText = '';
-  
+
   // switch statement to determine which table to update
-  switch(sectionName) {
-    case 'Guidelines': 
+  switch (sectionName) {
+    case 'Guidelines':
       sqlText = `
         UPDATE "guidelines" 
         SET "name" = $4 , "info" = $5, "grade" = $6, "date" = $7
         WHERE "id" = $1 AND "health_category_id" = $2 AND "age_range_id" = $3;`;
-      sqlParams = [req.body.id, req.params.catId, req.params.ageId, 
-        req.body.field01, req.body.field02, req.body.field03, req.body.field04];
-      
+      sqlParams = [req.body.id, req.params.catId, req.params.ageId,
+      req.body.field01, req.body.field02, req.body.field03, req.body.field04];
+
       break;
     case 'Diagnostic Tools':
       sqlText = `
         UPDATE "diagnostic_tool"
         SET "name" = $4 , "info" = $5
         WHERE "id" = $1 AND "health_category_id" = $2 AND "age_range_id" = $3;`;
-      sqlParams = [req.body.id, req.params.catId, req.params.ageId, 
-        req.body.field01, req.body.field02];
+      sqlParams = [req.body.id, req.params.catId, req.params.ageId,
+      req.body.field01, req.body.field02];
       break;
     case 'FAQ':
       sqlText = `
         UPDATE "faq"
         SET "question" = $4 , "answer" = $5
         WHERE "id" = $1 AND "health_category_id" = $2 AND "age_range_id" = $3;`;
-      sqlParams = [req.body.id, req.params.catId, req.params.ageId, 
-        req.body.field01, req.body.field02];
+      sqlParams = [req.body.id, req.params.catId, req.params.ageId,
+      req.body.field01, req.body.field02];
       break;
     case 'Questions for Your Doctor':
       sqlText = `
         UPDATE "doctor_questions"
         SET "question" = $5 , "question_category" = $4
         WHERE "id" = $1 AND "health_category_id" = $2 AND "age_range_id" = $3;`;
-      sqlParams = [req.body.id, req.params.catId, req.params.ageId, 
-        req.body.field01, req.body.field02];
+      sqlParams = [req.body.id, req.params.catId, req.params.ageId,
+      req.body.field01, req.body.field02];
       break;
     case 'Resources':
       sqlText = `
@@ -173,19 +172,19 @@ router.put('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, (req, res)
       ;
       `;
       sqlParams = [req.body.name, req.body.link, req.body.description, req.body.id, catId];
-    break;
+      break;
   }
-  
+
   console.log('sql params are', sqlParams);
 
   pool.query(sqlText, sqlParams)
-  .then(dbRes => {
-    res.sendStatus(204);
-  })
-  .catch(error => {
-    console.log(error);
-    res.sendStatus(500);
-  })
+    .then(dbRes => {
+      res.sendStatus(204);
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    })
 });
 
 
@@ -198,15 +197,15 @@ router.post('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, (req, res
 
   let sqlParams = [];
   let sqlText = '';
-  
+
   // switch statement to determine which table to update
-  switch(sectionName) {
-    case 'Guidelines': 
+  switch (sectionName) {
+    case 'Guidelines':
       sqlText = `
         INSERT INTO "guidelines" ("name", "info", "health_category_id", "age_range_id", "grade", "date")
         VALUES ($1, $2, $3, $4, $5, $6);`;
       sqlParams = [req.body.field01, req.body.field02, catId, ageId, req.body.field03, req.body.field04];
-      
+
       break;
     case 'Diagnostic Tools':
       sqlText = `
@@ -233,19 +232,19 @@ router.post('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, (req, res
       ;
       `;
       sqlParams = [req.body.field01, req.body.field02, req.body.field03, req.body.id, catId];
-    break;
+      break;
   }
 
   console.log('in add route; sql params are', sqlParams);
 
   pool.query(sqlText, sqlParams)
-  .then(dbRes => {
-    res.sendStatus(201);
-  })
-  .catch(error => {
-    console.log(error);
-    res.sendStatus(500);
-  })
+    .then(dbRes => {
+      res.sendStatus(201);
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    })
 });
 
 router.delete('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, (req, res) => {
@@ -255,48 +254,48 @@ router.delete('/:catId/ages/:ageId/:sectionName', rejectUnauthenticated, (req, r
 
   let sqlParams = [req.body.id];
   let sqlText = '';
-  
-    // switch statement to determine which table to delete from
-    switch(sectionName) {
-      case 'Guidelines': 
-        sqlText = `
+
+  // switch statement to determine which table to delete from
+  switch (sectionName) {
+    case 'Guidelines':
+      sqlText = `
           DELETE FROM "guidelines" 
           WHERE "id" = $1;`;
-        break;
-      case 'Diagnostic Tools':
-        sqlText = `
+      break;
+    case 'Diagnostic Tools':
+      sqlText = `
           DELETE FROM "diagnostic_tool" 
           WHERE "id" = $1;`;
-        break;
-      case 'FAQ':
-        sqlText = `
+      break;
+    case 'FAQ':
+      sqlText = `
           DELETE FROM "faq" 
           WHERE "id" = $1;`;
-        break;
-      case 'Questions for Your Doctor':
-        sqlText = `
+      break;
+    case 'Questions for Your Doctor':
+      sqlText = `
           DELETE FROM "doctor_questions" 
           WHERE "id" = $1;`;
-        break;
-      case 'Resources':
-        sqlText = `
+      break;
+    case 'Resources':
+      sqlText = `
           DELETE FROM "resources"
           WHERE "id" = $1
         ;
         `;
       break;
-    }
-    
-    console.log('deleting detail with id:', req.body.id);
-  
-    pool.query(sqlText, sqlParams)
-  .then(dbRes => {
-    res.sendStatus(204);
-  })
-  .catch(error => {
-    console.log(error);
-    res.sendStatus(500);
-  })
+  }
+
+  console.log('deleting detail with id:', req.body.id);
+
+  pool.query(sqlText, sqlParams)
+    .then(dbRes => {
+      res.sendStatus(204);
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    })
 });
 
 module.exports = router;
